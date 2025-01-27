a62611a16aa42ecf1d495f84161425f4fb8b4d02
avishkararjan <avishkar2108@gmail.com>
2025-01-27 17:38:51 +0530

    dependencies update

--- test.py
+++ test.py
@@ -1,41 +1,32 @@
 import requests
+import time
+
+t1 = time.time()
+# API URL
+url = "https://chromadb-vercel-test.onrender.com/"
+
+response = requests.get(url)
+print(response.json())
+
+t2 = time.time()
 
 # API URL
-url = "http://127.0.0.1:5000/"
+index_url = url+"/index-document"
+file_path = "./reliance_report.pdf"
+data = {
+    "share_name": "Reliance",
+    "date": "2025-01-25"
+}
+files = {
+    "file": open(file_path, "rb")
+}
 
-# Send GET request
-# response = requests.get(url)
+response = requests.post(index_url, data=data, files=files)
+print(response.json())
 
-# # Print the response
-# print(response.json())
+t3 = time.time()
 
-import requests
-
-# API URL
-# url = url+"/index-document"
-
-# # File path and metadata
-# file_path = "./reliance_report.pdf"
-# data = {
-#     "share_name": "Reliance",
-#     "date": "2025-01-25"
-# }
-
-# # Files to upload
-# files = {
-#     "file": open(file_path, "rb")
-# }
-
-# # Send POST request
-# response = requests.post(url, data=data, files=files)
-
-# # Print the response
-# print(response.json())
-
-# import requests
-
-# API URL
-url = url+"search"
+search_url = url+"search"
 print(url)
 
 # Query parameters
@@ -46,8 +37,14 @@ params = {
 }
 
 # Send GET request
-response = requests.get(url, params=params)
-
+response = requests.get(search_url, params=params)
 # Print the response
 print(response.json())
 
+t4 = time.time()
+
+print("------")
+print(f"/ time = {(t2-t1)}")
+print(f"/index-document time : {(t3-t2)}")
+print(f"/search : {(t4-t3)}")
+print(f"TOTAL : {t4-t1}")
